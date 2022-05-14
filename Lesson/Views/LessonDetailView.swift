083337ee6d4f35d845a193e163b8adbb1235bbd4
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-05-14 15:38:48 +0900

    group files

---
+++ Lesson/Views/LessonDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  LessonDetailView.swift
+//  Lesson
+//
+//  Created by 정호윤 on 2022/05/14.
+//
+
+import SwiftUI
+
+struct LessonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LessonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LessonDetailView()
+    }
+}
