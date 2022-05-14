f60865554b1b42a55bc47be849f7e734bd0c886d
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-05-14 15:13:20 +0900

    Initial Commit

---
+++ Lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson
+//
+//  Created by 정호윤 on 2022/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson/LessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonApp.swift
+//  Lesson
+//
+//  Created by 정호윤 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct LessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
